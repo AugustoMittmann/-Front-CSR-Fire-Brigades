396ae3e8e22d7cb3dbac9f6d31460f542ac1754a
Felipe Keller <felipeakeller@users.noreply.github.com>
2025-06-24 10:52:57 -0300

    Filter popup (#6)
    * inital commit
    
    * initial commit
    
    * add dependency
    
    * add filter and sorter
    
    * remove comments
    
    * refactoring
    
    * remove empty line
    
    * add filter to viewBrigadesPage

---
+++ src/app/home/components/dynamicRadioButtons.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styles from "./dynamicRadioButtons.module.css";
+
+const DynamicRadioButtons = ({ options, onSelect }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onSelect(value);
+  };
+
+  return (
+    <div style={
+        {width: "100%", justifyContent: "center", alignItems: "start"}}>      
+      {options.map((option) => (
+        <div key={option.value} style={{
+            width:"100%", 
+            display: "flex",
+            marginBottom: "6px",
+        }}>
+          <input
+            type="checkbox"
+            id={option.value}            
+            value={option.value}
+            checked={selectedOption === option.value}
+            onChange={handleOptionChange}
+            className={styles.radiobutton}
+          />
+          <label htmlFor={option.value} style={{
+            font: "normal normal 16px/24px 'Montserrat'"
+            }}>{option.label}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DynamicRadioButtons;
